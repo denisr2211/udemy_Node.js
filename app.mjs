dc3cff9b84f881be40ffeb8ac9d5d4f091ee6468
Den <denis.r2211@gmail.com>
2022-06-21 20:14:39 +0300

    export

---
+++ app.mjs
@@ -0,0 +1,13 @@
+// import log, * as char from './characters.mjs';
+
+async function main() {
+    try {
+        const { characters, greet } = await import('./characters.mjs')
+        for (const c of characters) {
+            greet(c);
+        }
+    } catch (e) {
+        console.log('Ошибка');
+    }
+}
+main();
