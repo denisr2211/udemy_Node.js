4cf3a8e98378509ae5aa0a9e73c06ad769319583
Den <denis.r2211@gmail.com>
2022-06-30 15:37:24 +0300

    CLI

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ xxkvmdln
 /////////////////////
 npm
 npm login
-npm whoami
+npm whoami // под кем залогинин
 npm profile get
 npm logout
 
@@ -23,3 +23,26 @@ git config core.autocrlf true - warning: LF will be replaced by CRLF in package-
 The file will have its original line endings in your working directory
 warning: LF will be replaced by CRLF in package.json.
 The file will have its original line endings in your working directory
+
+User@WIN-4L6FG2E10SM MINGW64 ~/Desktop/udemy_Node.js/WEATHER-CLI (master)
+$ node weather.js -t 290224fbc95846c9cada9af1d272d2a9
+ SUCCESS  Токен сохранён
+
+ User@WIN-4L6FG2E10SM MINGW64 ~/Desktop/udemy_Node.js/WEATHER-CLI (master)
+$ node weather.js -s kiev
+ SUCCESS  Город сохранён
+
+ User@WIN-4L6FG2E10SM MINGW64 ~/Desktop/udemy_Node.js/WEATHER-CLI (master)
+$ node weather.js
+ WEATHER  Погода в городе Киев
+☁ пасмурно
+Темпервтура: 18.86 (ощущается как 19.05)
+Влажность: 86%
+Скорость ветра: 0.45
+
+$ node weather.js
+ WEATHER  Погода в городе Днепр
+☀ ясно
+Темпервтура: 21.95 (ощущается как 21.87)
+Влажность: 64%
+Скорость ветра: 3.49
--- WEATHER-CLI/weather.js
+++ WEATHER-CLI/weather.js
@@ -1,18 +1,64 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY, getKeyValue } from './services/storage.service.js';
+
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан токен');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.token, token);
+        printSuccess('Токен сохранён');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const saveCity = async (city) => {
+    if (!city.length) {
+        printError('Не передан город');
+        return;
+    }
+    try {
+        await saveKeyValue(TOKEN_DICTIONARY.city, city);
+        printSuccess('Город сохранён');
+    } catch (e) {
+        printError(e.message);
+    }
+}
+
+const getForcast = async () => {
+    try {
+        const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city)
+        const weather = await getWeather(city);
+        printWeather(weather, getIcon(weather.weather[0].icon));  // красивый вывод погоды
+    } catch (e) {
+        if (e?.response?.status == 404) {
+            printError('Невено указан город');
+        } else if (e?.response?.status == 401) {
+            printError('Невено указан токен');
+        } else {
+            printError(e.message);
+        }
+    }
+}
 
 const initCLI = () => {
-    const args = getArgs(process.argv)
+    const args = getArgs(process.argv);
     if (args.h) {
-        printHelp();  // Вывод help        
+        return printHelp();  // Вывод help        
     }
     if (args.s) {
+        return saveCity(args.s);
         // Сохранить город      
     }
     if (args.t) {
-        // Сохранить токен        
+        return saveToken(args.t);   
     }
+    return getForcast();
     // Вывести погоду
 };
 
